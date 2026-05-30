e94480d6c810ef7ee126bc93b6022068a6d098d7
Pascal-Huang <pas.huang9@gmail.com>
2026-05-30 15:19:56 -0400

    feat: add SiteFooter to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "@/components/hero/HeroSection";
 import { ProductsSection } from "@/components/products/ProductsSection";
 import { EcosystemSection } from "@/components/ecosystem/EcosystemSection";
 import { CTASection } from "@/components/cta/CTASection";
+import { SiteFooter } from "@/components/layout/SiteFooter";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <ProductsSection />
       <EcosystemSection />
       <CTASection />
+      <SiteFooter />
     </main>
   );
 }
